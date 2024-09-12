09a231d51f1842dba040a08a359e1ebfb3ba26c6
aboubacar toure <aboubacartoure93@gmail.com>
2024-09-12 10:17:41 -0400

    hotwire working
    hotwire working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,48 @@
 class CommentsController < ApplicationController
 
   before_action :set_post	
+  before_action :set_comment, only:[:edit, :update, :destroy]
   before_action :authenticate_user!	
   
   def create
   	@comment = @post.comments.new(comment_params.merge(user: current_user))
-
   	respond_to do |format|
   		if @comment.save
         format.turbo_stream
-  			format.html { redirect_to post_url(@post), 
-  				notice: "Comment was successfully created."}
-  			format.json { render :show, status: :created, location: @comment }
-
+  			format.html { redirect_to post_url(@post), notice: "Comment was successfully created."}
   		else
         format.turbo_stream
   			format.html { render :new, status: :unprocessable_entity }
-  				
-  			format.json { render json: @comment.errors, status: :unprocessable_entity }
   		end
   	end
-  			
   end
 
 
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @comment.update(comment_params)
+          format.turbo_stream
+          format.html { redirect_to post_url(@post), notice: "Comment was successfully created."}
+        else
+          format.turbo_stream
+          format.html { render :edit, status: :unprocessable_entity }
+        end 
+      end
+    end
+
+    def destroy
+    end
+  			
+
+
   private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 
 	  def set_post
 	  	@post = Post.find(params[:post_id])
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "./controllers"
 import "@hotwired/turbo-rails"
 import "trix"
 import "@rails/actiontext"
+
+
+
+
+// if (Rails.root.join("app/javascript/application.js")).exist?
+// say "Import Stimulus controllers"
+// append_to_file "app/javascript/application.js", %(import "./controllers"\n)
+// else
+// say %(You must import "./controllers" in your JavaScript entrypoint), :red
+// end
